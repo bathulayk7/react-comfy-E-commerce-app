0a0af6af2757f28e2ee59832bedbd5d35aa4e020
bathulayedukondalu <bathulayedukondalu@gmail.com>
2021-05-14 14:55:58 +0530

    first commit

---
+++ src/context/user_context.js
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const UserContext = React.createContext();
+export const UserProvider = ({ children }) => {
+  // isAuthenticated, isLoading
+  const { loginWithRedirect, logout, user } = useAuth0();
+  const [myUser, setMyUser] = useState(null);
+  useEffect(() => {
+    setMyUser(user);
+    // console.log(`user:${user}`);
+    // console.log(`isAuthenticated:${isAuthenticated}`);
+    // console.log(`isloading:${isLoading}`);
+    // if(isAuthenticated){
+    //   setMyUser(user)
+    // }
+    // else{
+    //   setMyUser(false)
+    // }
+    // }, [isAuthenticated]);
+  }, [user]);
+
+  return (
+    <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+// make sure use
+export const useUserContext = () => {
+  return useContext(UserContext);
+};
---
+++ src/utils/helpers.js
@@ -0,0 +1,20 @@
+export const formatPrice = (number) => {
+  return new Intl.NumberFormat("en-us", {
+    style: "currency",
+    currency: "USD",
+  }).format(number / 100);
+
+  // const newNumber=Intl.NumberFormat("en-us",{
+  //     style:"currency",
+  //     currency:"USD"
+  // }).format(number/100)
+  // return newNumber
+};
+
+export const getUniqueValues = (data, type) => {
+  let unique = data.map((item) => item[type]);
+  if(type==="colors"){
+    unique=unique.flat()
+  }
+  return ["all", ...new Set(unique)];
+};
